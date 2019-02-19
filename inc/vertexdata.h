d3eb2cc235f41328c14efa669c967d4d05fc523d
John Connors <johnfredcee@gmail.com>
2019-02-19 08:11:44 +0000

    Compiles again

--- inc/vertexdata.h
+++ inc/vertexdata.h
@@ -1,49 +1,49 @@
 #pragma once
 #include <string>
 #include <memory>
+#include <unordered_map>
 #include "buffer.h"
 
 #include <tuple>      // std::tuple
 #include <functional> // std::invoke
 
-template <
-    size_t Index = 0, // start iteration at 0 index
-    typename TTuple,  // the tuple type
-    size_t Size =
-        std::tuple_size_v<
-            std::remove_reference_t<TTuple>>, // tuple size
-    typename TCallable, // the callable to bo invoked for each tuple item
-    typename... TArgs   // other arguments to be passed to the callable 
->
-void map_tuple(TTuple&& tuple, TCallable&& callable, TArgs&&... args)
+class VertexAttribute
 {
-    if constexpr (Index < Size)
-    {
-        std::invoke(callable, args..., std::get<Index>(tuple));
-
-        if constexpr (Index + 1 < Size)
-            for_each<Index + 1>(
-                std::forward<TTuple>(tuple),
-                std::forward<TCallable>(callable),
-                std::forward<TArgs>(args)...);
-    }
-}
-
-template <class... Ts>
-struct VertexData
-{
-	template <typename T>
-	struct VertexDataBuffer
-	{
-		std::string name;
-		std::shared_ptr<Buffer<T>> data;
-	};
-
-	template<class... Ts>
-	using buffers = std::tuple<Ts...>;
-
-	VertexData(Ts... ts)
-	{
-		buffers = std::make_tuple(ts...);
-	}
+	friend class VertexData;
+public:
+	GLenum  type;
+	GLsizei offset;
+	GLsizei count;
+	void    *data;
 };
+
+template<typename T>
+struct VertexAttributeInitialiser
+{
+	using element_type = typename T;
+	using component_type = typename T::type;
+
+	const std::string name;
+	GLsizei count;
+	static constexpr GLsizei componentCount = element_type::dim;
+	static constexpr GLenum  type = GL_enum<component_type>::value;
+};
+
+class VertexData
+{
+ 	void    *data;
+	 GLsizei size;
+	std::unordered_map<std::string, VertexAttribute> attributes;
+};
+
+ template<typename T>
+ GLsizei addAttributeToVertexData(const VertexAttributeInitialiser<typename T>& initializer, GLsizei offset)
+ {
+	VertexAttribute attribute{ initializer.type, offset, initializer.count, nullptr };
+	attributes[initializer.name] = attribute;
+	constexpr GLsizei typeSize = sizeof(initializer.type);
+	return offset +  count * typeSize * initializer.count;
+ }
+
+ 
+ 
