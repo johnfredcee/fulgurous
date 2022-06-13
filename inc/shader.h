7e6c93a82884d9e6ceadf7545f5461f1f7de9b5f
John Connors <johnfredcee@gmail.com>
2022-06-13 13:55:13 +0100

    Tidy up the project, prior to further development

--- inc/shader.h
+++ inc/shader.h
@@ -9,38 +9,17 @@ enum ShaderKind : GLuint
     eSHADER_COUNT    = 3  
 };
 
-class ShaderProgram
+class Shader
 {
 public:
-    struct ShaderParameter
-    {
-        GLuint location;
-        std::string name;
-    };
-    ShaderProgram(); 
-    ~ShaderProgram();
+    Shader(ShaderKind inKind);
+    ~Shader();
+    void load_from_string(const std::string& filename);
+    void load_from_file(const std::string& filename);
+    void compile();
+    bool verify();
+protected:
+    ShaderKind kind;
+    const GLuint glShaderConstants[ShaderKind::eSHADER_COUNT] = { GL_VERTEX_SHADER, GL_FRAGMENT_SHADER, GL_GEOMETRY_SHADER };
 
-    void use();    
-    void unuse();
-
-    void load_from_string(ShaderKind kind,  const std::string& source);
-    void load_from_file(ShaderKind kind, const std::string& filename);
-
-    void compile(ShaderKind kind);
-
-    void link();
-
-    GLint attribute_location(const std::string& name);
-    GLint uniform_location(const std::string& name);
-    
-    std::vector<ShaderParameter> uniforms;
-    std::vector<ShaderParameter> attributes;
-    
-private:
-
-    void gather_attributes();
-    void gather_uniforms();
-
-    GLuint shaders[eSHADER_COUNT];
-    GLuint program;
 };
